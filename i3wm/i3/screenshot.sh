86eaf94e2a24ca23365b527516a60def3daca505
linyuan <2551487871@qq.com>
2019-05-07 16:42:39 +0800

    i3wm tmux配置

---
+++ i3wm/i3/screenshot.sh
@@ -0,0 +1,37 @@
+
+pic_dir=$HOME/Pictures
+# screenshot_dir=${pic_dir}/ScreenShots
+screenshot_png=${pic_dir}/SCREENSHOT.png
+flameshot_dir=${pic_dir}/FlameShots
+flameshot_png=${pic_dir}/SCREENSHOT_FLAME.png
+
+after() {
+	xclip -selection clipboard -t image/png -i "$screenshot_png"
+	prompt
+}
+	
+flameshot_before() {
+	if [[ -e $flameshot_png ]]; then
+		rm -vf "${flameshot_png}"
+	fi
+}
+					
+do_scrot() {
+	scrot $1 $screenshot_png
+}
+					
+flameshot_area() {
+	flameshot_before 
+	flameshot gui -p $pic_dir
+}
+						
+prompt() {
+	t=`date +%T`
+	notify-send "Saved as $screenshot_png [${t}]"
+}
+							
+case $1 in
+	full    ) do_scrot && after    ;;
+	area    ) do_scrot -s && after ;;
+	area_fs ) flameshot_area       ;;
+esac
